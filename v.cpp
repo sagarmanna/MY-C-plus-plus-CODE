654a8762accadc87b3dcd93650fb4d43c9cb235a
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2024-08-16 21:49:37 +0530

    Add files via upload
    pattern ques 2

--- v.cpp
+++ v.cpp
@@ -4,19 +4,28 @@
 using namespace std;
 
 int main(){
+/*
+* 
+* *
+* * *
+* * * *
+* * * * *
+*/
 
-
-//1 no, Question
-// * * * * * 
-// * * * * *
-// * * * * *
-// * * * * *
-// * * * * *
-
-  for(int i=1;i<=5;i++){
-    for(int j=1;j<=5;j++){
-      cout<<"* ";
-    }
-    cout<<endl;
+for(int i =1; i<=5;i++){
+  for(int j=1;j<=i;j++){
+    cout<<"* ";
   }
+  cout<<endl;
 }
+  }
+
+
+
+
+
+
+
+
+
+
