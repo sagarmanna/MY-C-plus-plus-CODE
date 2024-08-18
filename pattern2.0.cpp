28fcc40ff3246094454778de10cd3f641e3e615c
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2024-08-18 21:13:20 +0530

    Add files via upload

--- pattern2.0.cpp
+++ pattern2.0.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 int main(){
 
+/*
+      *
+     **
+    ***
+   ****
+  *****
+*/
 
 for(int i =0;i<5;i++){
     for(int j=0;j<=5-i;j++){
@@ -12,5 +19,21 @@ for(int i =0;i<5;i++){
     }
     cout<<endl;
 }
+
+//       * 
+//      * *
+//     * * *
+//    * * * *
+//   * * * * *
+for(int i =0;i<5;i++){
+    for(int j=0;j<=5-i;j++){
+        cout<<" ";
+    }
+    for(int j=0;j<=i;j++){
+        cout<<"* ";
+    }
+    cout<<endl;
+}
+
 
 }
