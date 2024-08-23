b25b90fce42639bd98f84490edd871302ca83016
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2024-08-23 23:34:34 +0530

    Add files via upload

---
+++ new array.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+ int main(){
+
+    int arr[1000];
+    for(int i=0;i<1000;i++){
+        cin>>arr[i];
+        
+    }
+
+    //print arr
+    for(int i=0;i<5;i++){
+        cout<<arr[i];
+    }
+
+        
+
+ }
