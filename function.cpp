b4076fb292be724ec6f43130b571eaf916656312
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2024-08-25 12:27:42 +0530

    Add files via upload

---
+++ function.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+
+void printname(){
+    int n;
+    cout<<"Enter n:";
+    cin>>n;
+    for(int i =0;i <=n;i++){
+        cout<<"SAGAR"<< "   ";
+    }
+}
+int main(){
+printname();
+}
