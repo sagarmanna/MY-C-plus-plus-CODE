e4174afdee8ab537487f8fe3590ff50c97c53e29
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2024-08-29 23:17:32 +0530

    Add files via upload

---
+++ Array.coder Ques.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //search element 
+int x=8;
+int x;
+    for(int i =0;i<=5;i++){
+        if(arr[i]==x){
+            index-i;
+        }
+    }
+}
