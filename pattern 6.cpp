a2916190bcb5bdf3b06016df7c8ae54bd8586a54
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2024-08-31 16:23:33 +0530

    Add files via upload

---
+++ pattern 6.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i=0;i<4;i++){
+        for(int j=0;j<5;j++){
+            cout<<"0"<<" ";
+        }
+        cout<<endl;
+    }
+}
+
+
+/*
+ANS: 
+
+0 0 0 0 0 
+0 0 0 0 0
+0 0 0 0 0
+0 0 0 0 0
+
+
+
+*/
