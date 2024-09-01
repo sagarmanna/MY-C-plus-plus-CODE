e40afd1274933154b105edbff2f2c946f5a1472d
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2024-09-01 22:17:31 +0530

    Add files via upload

--- pattern 8.cpp
+++ pattern 8.cpp
@@ -1,8 +1,10 @@
 #include<iostream>
 using namespace std;
 int main(){
-
-for(int i =1;i<=4;i++){
+int num;
+cout<<"Enter name: ";
+cin>>num;
+for(int i =1;i<=num;i++){
     for(int j=1;j<=i;j++){
         cout<<j<<" ";
     }
