ec500a544b82594bab63aa4c23e93b460a25f4d7
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2024-09-03 23:09:08 +0530

    Add files via upload

--- function11.cpp
+++ function11.cpp
@@ -2,17 +2,21 @@
 using namespace std;
 int main(){
 
-for(int i=0;i<5;i++){
-    for(int j=0;j<=i;j++){
-        cout<<"* ";
+for(int i=1; i<=6;i++){
+    for(int j =i;j>=1;j--){
+        cout<<i<<" ";
     }
     cout<<endl;
+}
 
  
 }
 
 
+// 4 4 4 4 
+// 3 3 3
+// 2 2 
+// 1
 
 
 
-}
