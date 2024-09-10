eb0701a15127e413b1453ec350fe06bcee6579d5
Sagar Manna <121675250+sagarmanna@users.noreply.github.com>
2024-09-10 21:19:28 +0530

    Add files via upload

---
+++ pattern 20.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main(){
+    for(int i =0;i<5;i++){
+        for(int j =0;j<=i;j++){
+            cout<<"0 ";
+        }
+        cout<<endl;
+    }
+}
+
+/*
+
+0 
+0 0
+0 0 0
+0 0 0 0
+0 0 0 0 0
+
+
+*/
